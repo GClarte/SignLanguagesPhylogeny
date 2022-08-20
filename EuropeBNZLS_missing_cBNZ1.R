6de09d7a67e858c4f6eb933092bf86c88d41306c
Grégoire Clarté <47453964+GClarte@users.noreply.github.com>
2022-08-20 11:06:26 +0300

    Add files via upload

---
+++ EuropeBNZLS_missing_cBNZ1.R
@@ -0,0 +1,257 @@
+#JJ=read.csv("datasets/fulldataavecasie.csv",header=T)
+J=read.csv("datasets/datafincorr.csv",header=T,stringsAsFactors =T, na.strings="")
+
+source("fonctions/fctauxini.R")
+source("fonctions/gibbsparam.R")
+source("fonctions/gibbstps.R")
+source("fonctions/gibbstranfter.R")
+source("fonctions/initialisation.R")
+source("fonctions/lkldpruningbis.R")
+source("fonctions/simudata.R")
+source("fonctions/Gibbspartieldata.R")
+source("fonctions/gibbsrho.R")
+source("fonctions/modiftopo.R")
+source("fonctions/SMCforet.R")
+source("fonctions/SMCforet_aux.R")
+source("fonctions/testclades.R")
+
+library("parallel")
+
+
+
+# levels(J$handshape)=c(levels(J$handshape),"L","M","Autre")
+# 
+# J$handshape[J$handshape=="1_b"]="1"
+# J$handshape[J$handshape=="1_nb"]="1"
+# J$handshape[J$handshape=="4_nb"]="4"
+# J$handshape[J$handshape=="5_b"]="5"
+# J$handshape[J$handshape=="5_nb"]="5"
+# J$handshape[J$handshape=="B_b"]="B"
+# J$handshape[J$handshape=="B_nb"]="B"
+# J$handshape[J$handshape=="E_b"]="E"
+# J$handshape[J$handshape=="E_bnb"]="E"
+# J$handshape[J$handshape=="E_nb"]="E"
+# J$handshape[J$handshape=="L_b"]="L"
+# J$handshape[J$handshape=="L_bnb"]="L"
+# J$handshape[J$handshape=="L_eb"]="L"
+# J$handshape[J$handshape=="L_ebnb"]="L"
+# J$handshape[J$handshape=="L_enb"]="L"
+# J$handshape[J$handshape=="L_nb"]="L"
+# J$handshape[J$handshape=="M_enb"]="M"
+# J$handshape[J$handshape=="M_eb"]="M"
+# J$handshape[J$handshape=="S_b"]="S"
+# J$handshape[J$handshape=="V_nb"]="V"
+# 
+# J$handshape[J$handshape=="3_b"]="Autre"
+# J$handshape[J$handshape=="3"]="Autre"
+# J$handshape[J$handshape=="4"]="Autre"
+# J$handshape[J$handshape=="6"]="Autre"
+# J$handshape[J$handshape=="7_enb"]="Autre"
+# J$handshape[J$handshape=="K"]="Autre"
+# J$handshape[J$handshape=="Q"]="Autre"
+# J$handshape[J$handshape=="R"]="Autre"
+# J$handshape[J$handshape=="U"]="Autre"
+# J$handshape[J$handshape=="U_b"]="Autre"
+# J$handshape[J$handshape=="U_nb"]="Autre"
+# J$handshape[J$handshape=="W_nb"]="Autre"
+# J$handshape[J$handshape=="W"]="Autre"
+# J$handshape[J$handshape=="X"]="Autre"
+# 
+# J$handshape[J$handshape=="M"]="Autre"
+# J$handshape[J$handshape=="Y"]="Autre"
+# J$handshape[J$handshape=="I"]="Autre"
+# J$handshape[J$handshape=="H"]="Autre"
+# 
+# J$handshape=droplevels(J$handshape)
+
+qui=c(7,28,29,6,10)
+#qui=c(7,28,20,10)
+#qui=c(7,28,20,6)
+#langues européennes
+quelleslangues=c("Lithuanian","Czech","German","French","Russian","AmericanEnglish","Italian","Austrian","Ukrainian","Spanish","Polish",
+                 "English","NewZealand","Estonian","Latvian","AllMissing")
+
+nlangues=length(quelleslangues)
+
+rdirichlet <- function (n, alpha) 
+{
+  l <- length(alpha)
+  x <- matrix(rgamma(l * n, alpha), ncol = l, byrow = TRUE)
+  sm <- x %*% rep(1, l)
+  return(x/as.vector(sm))
+}
+
+k=c()
+for (i in 1:100){
+  if (sum(as.integer(J[which(J[,4]%in%quelleslangues),3])==i)==nlangues & !any(is.na(sapply(1:3,function(z){sum(as.integer(J[as.integer(J[,1])==i & J[,2]%in%quelleslangues,qui[z]]))})))){
+    k=c(k,i)
+  }
+}
+
+quelmeanings=k
+
+quelmeanings=1:100
+
+nch=length(qui)
+
+Dtot=list()
+for (i in 1:nch){
+  Dtot[[i]]=matrix(ncol=nlangues,nrow=length(quelmeanings))
+  for (j in 1:length(quelleslangues)){
+    for (l in 1:length(quelmeanings)){
+      if (length(J[which(J[,4]==quelleslangues[j]& as.integer(J[,3])==quelmeanings[l]),qui[i]])>0){
+        Dtot[[i]][l,j]=as.integer(J[which(J[,4]==quelleslangues[j]& as.integer(J[,3])==quelmeanings[l]),qui[i]])
+      } else {
+        Dtot[[i]][l,j]=NA
+      }
+    }
+      
+
+  }
+}
+
+
+toutestransf=function(n){
+  A=matrix(nrow=2,ncol=0)
+  for (i in 1:n){
+    for (j in 1:n){
+      if (i!=j){
+        A=cbind(A,c(i,j))
+      }
+    }
+  }
+  return(A)
+}
+
+partopo=.001
+priorcoal <- function(tr,agemax){
+  tip=Prior$tipprior
+  if (!all(sapply(tipprior,function(tip){is.monophyletic.perso(statet$Tr,tip)}))){
+    return(-Inf)
+  } else {
+    L=sum(tr$edge.length)
+    n=length(tr$tip.label)
+    return(log((1-exp(-partopo*L/2))^(n-1)*(exp(partopo*t/2))))
+  }
+}
+
+etats=sapply(1:nch,function(x){length(levels(J[,qui[x]]))})
+passages=lapply(etats,function(x){S=toutestransf(x);return(split(S,col(S)))})
+
+priorbeta <- function(x,y){if(length(x$root[[2]])>0){0}else{dbeta((sum(x$edge.length)-2*y)/((length(x$tip.label)-2)*y),1,100,log=T)}}
+
+prioryule <- function(x,y){
+  tips=Prior$tipprior
+  if (!all(sapply(tips,function(tip){is.monophyletic.perso(x,tip)}))){
+    return(-Inf)
+  } else {
+  if(length(x$root[[2]]>0)){
+    K=sapply(x$root[[2]],function(z){y-hauteur(x,z)})
+    return(-1*length(x$tip.label)*log(sum(x$edge.length)+sum(K)))
+  } else {
+    return(-1*length(x$tip.label)*log(sum(x$edge.length)))
+  }}
+}
+
+priorunif <- function(tr,agemax){
+  tips=Prior$tipprior
+  cladeage=Param$Cladeage
+  if (!all(sapply(tips,function(tip){is.monophyletic.perso(tr,tip)})) ||
+      !all(sapply(cladeage,function(clade){ageconstraints(tr,clade)}))){
+    return(-Inf)
+  } else {
+    hhh=hauteur(tr,tr$root[[2]])
+    if (!(hhh>150&hhh<1000)){
+      return(-Inf)
+    } else {
+      return(-length(tr$tip.label)*log(hauteur(tr,tr$root[[2]])))
+    }
+  }
+}
+
+#paramètres
+
+ncogntot=nrow(Dtot[[1]])
+
+nch=length(etats)
+Dat=Dtot
+hyperpbini=lapply(passages,function(x){rep(1,length(x))})
+#hyperpbini=list(2,10,2,20)
+Pri=list(c(1,500,0,1),c(1,500,0,1),c(1,500,0,1),c(1,500,0,1),c(1,500,0,1))
+# Prirho=list(function(x){dgamma(x,1,5000,log=T)
+# },list(
+#   function(){rgamma(1,1,5000)},
+#   function(x){dgamma(x,1,5000,log=T)}
+# ))
+Prirho=c(1,500)
+loiini=lapply(etats,function(x){rep(1/x,x)})
+pribruit=list(list(function(){runif(1,10^-5,10^-3)},function(x){
+  if(x>10^-5 & x<10^-3){
+    return(-log(x))
+  } else {
+    return(-Inf)
+  }
+}),
+list(function(x){rnorm(1,x,.001)},
+     function(x,y){dnorm(x,y,.001,log=T)}))
+              #list(function(x){exp(log(x)+rnorm(1,0,.00001))},
+              # function(x,y){-(log(y)-log(x))/2*.00001-log(x)}))
+
+#BNZ=which(quelleslangues%in%c("English","NewZealand"))
+
+ages=matrix(c(1,2,3,4,5,6,7,8,9,10,
+              127,216,122,145,194,183,216,221,195,205),ncol=2)
+
+convcontraintes <- function(ages){
+  res=list()
+  cpt=1
+  for (i in 1:(nrow(ages)-1)){
+    for (j in (i+1):nrow(ages)){
+      res[[cpt]]=list(c(ages[i,1],ages[j,1]),c(min(ages[i,2],ages[j,2])-10,Inf))
+      cpt=cpt+1
+    }
+  }
+  return(res)
+}
+
+#Contraintesages=convcontraintes(ages)
+#Contraintesages=list(list(c(4,6),c(158,183)))
+#Contraintesages=list(list(c(4,7),c(181,216)))
+Contraintesages=list(list(c(12,13),c(107,132)))
+#Contraintesages=list(list(c(12,13),c(97,122)))
+
+
+Prior=list(Prirho=Prirho,
+           hyperpbini=hyperpbini,
+           Prila=Pri,
+           #priortree=prioryule,
+           priortree=priorunif,
+           #pribeta=c(.9,.9,.9,.9),
+           pribeta=matrix(rep(c(10,1),nch),ncol=nch,nrow=2),
+           #bruittemp=c(.1,.0005),
+           bruittemp=c(.1,.05,.03,.02,.01,.009,.005,.001,.0005),
+           pribruit=pribruit,#prior sur le bruit avec tout défini
+           tipprior=list(1:15))
+           #tipprior=list())
+  Param=list(Trpossliste=passages,
+           nch=nch,nph=etats,
+           npart=1500,
+           npas=4000,
+           npasfin=40000,
+           Nmin=750,
+           loiini=loiini,
+           Prob=c(1,.01,.1,.5,1,0,.01),
+           Probfin=c(1,.01,.1,.5,1,.1,.01),
+           agemax=c(100,1/2.5),
+           tiplabel=quelleslangues,
+           batches=c(lapply(1:(nlangues),function(x){ncogntot})),
+           ReconstructClade=list(),
+           Cladeage=Contraintesages
+)
+
+#Dat=lapply(1:3,function(x){matrix(NA,ncol=15,nrow=100)})
+  
+VV2=SMCbruit(Dat,Param,Prior,29)
+
+  
+save.image(paste("EuropeBNZ_missing_cBNZ1", date(), ".RData"), version = 2)
---
+++ GiveATry.R
@@ -0,0 +1,207 @@
+#you only need the parallel package, and all the functions that we wrote
+
+#if you have a question send me an e-mail at: clarte $at$ ceremade $dot$ dauphiine $dot$ fr or at clarteg $at$ gmail $dot$ com
+
+
+source("fonctions/fctauxini.R")
+source("fonctions/gibbsparam.R")
+source("fonctions/gibbstps.R")
+source("fonctions/gibbstranfter.R")
+source("fonctions/initialisation.R")
+source("fonctions/lkldpruningbis.R")
+source("fonctions/simudata.R")
+source("fonctions/Gibbspartieldata.R")
+source("fonctions/gibbsrho.R")
+source("fonctions/modiftopo.R")
+source("fonctions/SMCforet.R")
+source("fonctions/SMCforet_aux.R")
+source("fonctions/testclades.R")
+
+library("parallel")
+
+#========================================================================================
+#                             Preparing the Data
+#=========================================================================================
+
+#The dataset comes in the form of a CSV file, as presented in example_data.csv
+
+#vector of name of the languages, in the order of the dataset, 
+#it also gives the name of the leaves of the tree, the number in the constraints
+quelleslangues = 
+
+nlangues=length(quelleslangues)
+
+#vector of the indexes of the meanings
+quelmeanings=1:100
+
+#number of characters in the dataset
+nch=length(qui)
+
+#The dataset must be transformed in order to have a list of length nch
+#each element of the list is a matrix with nlanguages columns and a row for each meaning
+#NA are possible in any of the matrices
+Dat
+
+#this function creates all the pairwise transformations possible. This is not optimal and we don't care.
+toutestransf=function(n){
+  A=matrix(nrow=2,ncol=0)
+  for (i in 1:n){
+    for (j in 1:n){
+      if (i!=j){
+        A=cbind(A,c(i,j))
+      }
+    }
+  }
+  return(A)
+}
+
+
+#etats is a vector of size nch containing the number of values possible for each character
+etats
+
+#passages is a list of length nch, for each character it gives the possible transformations
+#for each character the possible transformation is a list of vectors of size 2 giving the starting and endind value of the transformation
+#the following code transforms "etats" in order to have all pairwise transformations possible
+passages=lapply(etats,function(x){S=toutestransf(x);return(split(S,col(S)))})
+
+#uniform prior, always useful, we use this one
+priorunif <- function(tr,agemax){
+  tips=Prior$tipprior
+  cladeage=Param$Cladeage
+  if (!all(sapply(tips,function(tip){is.monophyletic.perso(tr,tip)})) ||
+      !all(sapply(cladeage,function(clade){ageconstraints(tr,clade)}))){#tests for the constrains on clades and on internal ages
+    return(-Inf)
+  } else {
+    hhh=hauteur(tr,tr$root[[2]])
+    if (!(hhh>200&hhh<1000)){#add here the constrains on the root age.
+      return(-Inf)
+    } else {
+      if (length(cladeage)==0){
+        return(-length(tr$tip.label)*log(hauteur(tr,tr$root[[2]])))
+      } else {
+        return(-(length(tr$tip.label)-sum(sapply(cladeage,function(x){length(x[[1]])-1})))*log(hauteur(tr,tr$root[[2]])))
+      }
+    }
+  }
+}
+
+#with gamma prior on the length of the tree, same as before, on simulated data it gives quite similar results
+priorgamma <- function(tr,agemax){
+  tips=Prior$tipprior
+  cladeage=Param$Cladeage
+  if (!all(sapply(tips,function(tip){is.monophyletic.perso(tr,tip)})) ||
+      !all(sapply(cladeage,function(clade){ageconstraints(tr,clade)}))){
+    return(-Inf)
+  } else {
+    hhh=hauteur(tr,tr$root[[2]])
+    if (!(hhh>200&hhh<1000)){
+      return(-Inf)
+    } else {
+      if (length(cladeage)==0){
+        return(-length(tr$tip.label)*log(hauteur(tr,tr$root[[2]]))+
+                 dgamma(sum(tr$edge.length),10,1/90,log=T))
+      } else {
+        return(-(length(tr$tip.label)-sum(sapply(cladeage,function(x){length(x[[1]])-1})))*log(hauteur(tr,tr$root[[2]]))+
+                 dgamma(sum(tr$edge.length),10,1/90,log=T))
+      }
+    }
+  }
+}
+
+
+#parameters of the algorithm
+
+#number of meanings
+ncogntot=nrow(Dtot[[1]])
+#of characters
+nch=length(etats)
+#data
+Dat
+#hyperparameter on the dirichlet distribution on the possible transformation, here uniform
+hyperpbini=lapply(passages,function(x){rep(1,length(x))})
+#prior on the transformation frequency, for each character, these are the parameter of a Gamma distribution
+#beware that we use this prior ONLY because we have chosen a certain value for the root age
+#in practice if the root age is around x, there should be in mean barplot(x*rgamma(1000,Pri[[i]][1],Pri[[i]][[2]]))
+#transformations for character i
+#here we propose this prior suitable for trees with root of age around a century, other parameters could be proposed,
+#the general format is : a list of length nch, each element is a 4 sized vector, the two first are the parameters of the gamma prior
+#and the last ones are the bound of the truncature.
+Pri=lapply(1:nch,function(x){c(1,500,0,1)})
+#prior on Rho, also Gamma, idem to check the number of apparitions PER MEANING.
+Prirho=c(1,5000)
+#initial distribution \pi_0 for each character, we choose a uniform distribution here
+loiini=lapply(etats,function(x){rep(1/x,x)})
+#prior on \nu the noise parameter, it is a list givint the computation of the prior and the proposal
+pribruit=list(list(function(){runif(1,10^-6,10^-2)},function(x){ #compute the value of the prior
+  if(x>10^-6 & x<10^-2){
+    return(-log(x))
+  } else {
+    return(-Inf)
+  }
+}),
+list(function(x){rnorm(1,x,.1)}, #an idoneous proposal
+     function(x,y){dnorm(x,y,.1,log=T)}))
+
+
+#we put everythin in two nice lists
+#in Prior all the prior informations
+Prior=list(Prirho=Prirho,#prior on Rho
+           hyperpbini=hyperpbini,#prior on the transf choice parameter
+           Prila=Pri,#prior on the transf rate
+           priortree=priorunif,#prior on the tree
+           pribeta=matrix(rep(c(10,1),nch),ncol=nch,nrow=2), #prior on beta the acceptance rate, here Beta(10,1)
+           #bruittemp=0.05-log(1:30)*(.05-.0005)/log(30),
+           bruittemp=c(.1,.05,.03,.02,.01,.009,.008,.007,.006,.005,.001),#decreasing scheme for the noise, the final value must be in a non-zero area of the function pribruit
+           pribruit=pribruit,#prior sur le bruit avec tout défini
+           tipprior=list())#here you can include several vectors indicating the indices of the leaves that must form a clade
+           #eg : tipprior=list(c(1,2,3),c(6,7))
+           #tipprior=list())
+#
+  Param=list(Trpossliste=passages,#possible transformations
+           nch=nch,nph=etats,
+           npart=3000,#number of particles. BEWARE, large value (over 4000 in my case) can cause memory overload, and R does not warn, see later
+           npas=5000,#number of step between each of the change in the noise value
+           npasfin=10000,#we finish with a very long MCMC
+           Nmin=2500,#if the ISS is smaller than Nmin, we resample, in practice we always resample
+           loiini=loiini,
+           Prob=c(1,.01,.1,.5,1,0,.01),#probability of the proposals in the MCMC when the noise is still fixes
+           #in the order : transformations, parameters, length of the edges, apparitions, topology, noise, beta
+           #the noise cannot change so Prob[7]=0
+           Probfin=c(1,.01,.1,.5,1,.1,.01),#when the noise can change Prob[7]!=0
+           agemax=c(100,1/3),#there are two possibilities, first you can fix it, giving a single number,
+           #or you can lear it, in which case you must put two aruyments that will be used as parameter of a gamma proposal
+           #just run hist(rgamma(1000,agemax[1],agemax[2])) and check the distribution of the rootage is close to what you want
+           tiplabel=quelleslangues,#label of the tips IN THE SAME ORDER THAN IN THE DATASET
+           batches=c(lapply(1:(nlangues),function(x){ncogntot})), #don't change that, even if it's not used anymore
+           ReconstructClade=list(),#list of vector of indices of the leaves whose common ancestor state must be reconstructed
+           Cladeage=list()#list of constraints on clade age, 
+           #eg : ReconstructClade=list(c(10,9),c(6,2)),
+           #Cladeage=list(list( c(10,9),c(10,200)),list( c(8,150),c(20,300)))#each element is a list whose first element is a vector of indices of leaves whose 
+           #common ancestor is constrained, and the second is the vector of lower and upper bound on the age
+)
+
+#4 parameters for the study. The three first are straightforward, the last one is the number of cores attributed on your cluster
+VV2=SMCbruit(Dat,Param,Prior,29)
+
+#The result should be a list of three elements the first one being of length npart
+#If you only get a list of npart elements, there has been an error somewhere and this error will appear in the list somewhere
+#if some elements of the lists contains NULL (ie are of length 0) there has been a memory overload, and you will have to decrease the number of particles
+
+#save the image as it is intended to run on a cluster.
+save.image(paste("test", date(), ".RData"), version = 2)
+
+#the first element of the list is the result proper. Each element corresponds to a particle final state.
+# For each particle you will find the following values :
+#Tr = the tree
+# La = lambda transformation rate
+# Rho = apparition rate
+# P the probability for each transformations
+#X = the nature and order of the transformations
+#Tps = the position of the transformations relatively to the edge
+#beta = value of the acceptance probability of the transformations
+#noise = the noise parameter
+#NL = the number of apparition gor each meaning for each edge
+#weight = the final weight of the particle
+
+#the second element containts the weights of each particle at each resampling step
+#the last one containts for each particle at each resampling step which particle was selected as ancestor.
